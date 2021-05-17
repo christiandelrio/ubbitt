94232b3b157b5df93455735ba62fb77b0412aac0
Christian Del Rio <christian.delrio@arkdiasolutions.com>
2021-05-17 13:11:54 -0500

    Agrega lógica para el cierre de sesión, actualizar los datos del usuario y recuperarlos

--- api/business/user_business.py
+++ api/business/user_business.py
@@ -3,6 +3,7 @@ from dao.session_dao import SessionDao
 from utils.crypto import Crypto
 from model.user import User
 from dao.user_dao import UserDao
+import copy
 import uuid
 
 class UserBusiness():
@@ -10,12 +11,14 @@ class UserBusiness():
         self.users_dao = UserDao()
         self.session_dao = SessionDao()
 
+    # Crea un nuevo usuario
     def create_user(self, username, email, password):
         self.validate_user_data(username, email, password)
         crypto = Crypto()
         hashed_password = crypto.cypher(password)
         return self.users_dao.create_user(User(username, email, hashed_password))
 
+    # Valida que la información en viada para crear un nuevo usuario sea válida
     def validate_user_data(self, username, email, password):
         if not username:
             raise Exception('El nombre de usuario no puede ser vacío.')
@@ -28,6 +31,7 @@ class UserBusiness():
         if self.users_dao.username_exists(username):
             raise Exception('No se puede crear el usuario, el nombre de usuario ya ha sido usado para otro usuario.')
 
+    # Inicia la sesión del usuario
     def login(self, username, password):
         user = self.users_dao.find_by_username(username)
         if user is None:
@@ -40,6 +44,14 @@ class UserBusiness():
         self.session_dao.save_session(session)
         return session_uuid
 
+    # Cierra la sesión del usuario
+    def logout(self, session_id):
+        session = self.session_dao.find_user_by_session(session_id)
+        if session is None:
+            raise Exception('La sesión es inválida.')
+        self.session_dao.delete_session_by_id(session_id)
+
+    # Actualiza la contraseña del usuario
     def update_password(self, session_id, new_password):
         session = self.session_dao.find_user_by_session(session_id)
         if session is None:
@@ -47,3 +59,19 @@ class UserBusiness():
         crypto = Crypto()
         new_hashed_password = crypto.cypher(new_password)
         self.users_dao.update_user_password(session.username, new_hashed_password)
+
+    # Actualiza la información del usuario
+    def update_user_data(self, session_id, email):
+        session = self.session_dao.find_user_by_session(session_id)
+        if session is None:
+            raise Exception('La sesión es inválida.')
+        self.users_dao.update_user_data(session.username, email)
+
+    # Recupera los datos del usuario excepto la contraseña
+    def get_user_data(self, session_id):
+        session = self.session_dao.find_user_by_session(session_id)
+        if session is None:
+            raise Exception('La sesión es inválida.')
+        user = copy.copy(self.users_dao.find_by_username(session.username))
+        del user.password
+        return user
