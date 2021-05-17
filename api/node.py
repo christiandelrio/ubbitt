80e86298d394ab05a8bfee021f15347a4bcfe217
Christian Del Rio <christian.delrio@arkdiasolutions.com>
2021-05-17 13:12:20 -0500

    Agrega manejo de peticiones para el cierre de sesión y recuperación de datos del usaurio

--- api/node.py
+++ api/node.py
@@ -29,10 +29,28 @@ def login():
         return jsonify({'message': str(err)}), 400
     return jsonify({'token': session_uuid}), 200
 
+# Logout
+@app.route('/logout', methods=["POST"])
+def logout():
+    try:
+        userBusiness.logout(request.headers['token'])
+    except Exception as err:
+        return jsonify({'message': str(err)}), 400
+    return jsonify({'message': 'Sesion cerrada correctamente'}), 200
+
 # Actualización de datos de usuario
 @app.route('/user', methods=["PATCH"])
 def update_user():
     return 'This works!'
+
+# Actualización de datos de usuario
+@app.route('/user', methods=["GET"])
+def get_user_data():
+    try:
+        user = userBusiness.get_user_data(request.headers['token'])
+    except Exception as err:
+        return jsonify({'message': str(err)}), 400
+    return jsonify(user.__dict__), 200
 
 # Cambio de contraseña
 @app.route('/user/password', methods=["PATCH"])
